930eb0546a0d300ab3c7de3cf6e7bb19fe894f41
Osha Foster <oshafoster@hotmail.com>
2017-11-28 10:48:56 -0700

    merge to master

--- exercises/messing-around/src/component/HomeComponent.js
+++ exercises/messing-around/src/component/HomeComponent.js
@@ -4,13 +4,18 @@ import {Link} from "react-router-dom"
 function HomeComponent(){
     return(
         <div className="home">
+            <div>
+            Home
+            <Link to="/main">Link</Link>
+            </div>
             <div className="box1">
                 <div className="photo">
-                    <img src="../../images/portrait.jpg" alt="profile"/>
-                </div>
+                    
             </div>
-            Hello World
-            <Link to="/main">Link</Link>
+            </div>
+            <div className="box2">
+                <div>Resume</div>
+            </div>
         </div>
     )
 }
